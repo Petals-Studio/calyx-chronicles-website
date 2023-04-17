71c960066758351292a788156b45346ef327d8c9
Shubham Sharma <shubham@tartanhq.com>
2023-04-18 02:29:34 +0530

    fix: ts declaration for window

--- utils/getMobileOperatingSystem.ts
+++ utils/getMobileOperatingSystem.ts
@@ -1,3 +1,10 @@
+declare global {
+  interface Window {
+    opera: any;
+    MSStream: any;
+  }
+}
+
 /**
  * Determine the mobile operating system.
  * This function returns one of 'iOS', 'Android', 'Windows Phone', or 'unknown'.
@@ -9,16 +16,16 @@ function getMobileOperatingSystem() {
 
   // Windows Phone must come first because its UA also contains "Android"
   if (/windows phone/i.test(userAgent)) {
-      return "Windows Phone";
+    return "Windows Phone";
   }
 
   if (/android/i.test(userAgent)) {
-      return "Android";
+    return "Android";
   }
 
   // iOS detection from: http://stackoverflow.com/a/9039885/177710
   if (/iPad|iPhone|iPod/.test(userAgent) && !window?.MSStream) {
-      return "iOS";
+    return "iOS";
   }
 
   return "unknown";
