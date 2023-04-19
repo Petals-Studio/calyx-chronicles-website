a284b9a198913ecfabcf3cfddcd1df1a12064fe6
Abhishek Maurya <abhishek@MacBook-Pro.local>
2023-04-19 18:33:51 +0530

    added server for serving bundle

---
+++ server.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+
+const path = __dirname + '/out';
+const port = 4200;
+app.use(express.static(path));
+app.get('/privacy-policy', (req: Request, res: Response) => {
+    res.sendFile(path + '/privacy-policy.html');
+});
+app.get('/terms-of-service', (req: Request, res: Response) => {
+    res.sendFile(path + '/terms-of-service.html');
+});
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(path + '/index.html');
+});
+app.listen(port);
