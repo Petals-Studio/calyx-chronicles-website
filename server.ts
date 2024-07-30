5823b0a16e2d9705d036eeb7dc0ac75b6fd0540f
Abhishek Maurya <abhishek@MacBook-Pro.local>
2024-07-30 17:22:40 +0530

    assetlinks added for deep linking

--- server.ts
+++ server.ts
@@ -11,16 +11,16 @@ app.use(function(req, res, next) {
 });
 app.use(express.static(path));
 
-app.get('/privacy-policy', (req: Request, res: Response) => {
-    res.sendFile(path + '/privacy-policy.html');
-});
-app.get('/terms-of-service', (req: Request, res: Response) => {
-    res.sendFile(path + '/terms-of-service.html');
-});
-app.get('/about-us', (req: Request, res: Response) => {
-    res.sendFile(path + '/about-us.html');
-});
-app.get('/', (req: Request, res: Response) => {
-  res.sendFile(path + '/index.html');
-});
+// app.get('/privacy-policy', (req: Request, res: Response) => {
+//     res.sendFile(path + '/privacy-policy.html');
+// });
+// app.get('/terms-of-service', (req: Request, res: Response) => {
+//     res.sendFile(path + '/terms-of-service.html');
+// });
+// app.get('/about-us', (req: Request, res: Response) => {
+//     res.sendFile(path + '/about-us.html');
+// });
+// app.get('/', (req: Request, res: Response) => {
+//   res.sendFile(path + '/index.html');
+// });
 app.listen(port);
