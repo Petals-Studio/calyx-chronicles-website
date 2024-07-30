aa0026639e05dec35eff8e49c0e66e9ef1c85f27
Abhishek Maurya <abhishek@MacBook-Pro.local>
2024-07-30 17:47:51 +0530

    caching headers changes

--- server.ts
+++ server.ts
@@ -5,26 +5,31 @@ const app: Express = express();
 const path = __dirname + '/out';
 const port = 8080;
 
+const pages = ['privacy-policy', 'terms-of-service', 'about-us'];
+
 app.use(function(req, res, next) {
+    const isHtmlPage = !pages.every(page => !req.url.includes(page));
     if (req.url.includes('assetlinks.json')) {
-        res.setHeader('Cache-Control', 'public, must-revalidate, max-age=30')
+        res.setHeader('Cache-Control', 'public, must-revalidate, max-age=30');
+    } else if (isHtmlPage || req.url === '/') {
+        res.setHeader('Cache-Control', 'public, must-revalidate, max-age=30');
     } else {
-        res.setHeader('Cache-Control', 'public, max-age=86400')
+        res.setHeader('Cache-Control', 'public, max-age-86400');
     }
     next();
 });
 app.use(express.static(path));
 
-// app.get('/privacy-policy', (req: Request, res: Response) => {
-//     res.sendFile(path + '/privacy-policy.html');
-// });
-// app.get('/terms-of-service', (req: Request, res: Response) => {
-//     res.sendFile(path + '/terms-of-service.html');
-// });
-// app.get('/about-us', (req: Request, res: Response) => {
-//     res.sendFile(path + '/about-us.html');
-// });
-// app.get('/', (req: Request, res: Response) => {
-//   res.sendFile(path + '/index.html');
-// });
+app.get('/privacy-policy', (req: Request, res: Response) => {
+    res.sendFile(path + '/privacy-policy.html');
+});
+app.get('/terms-of-service', (req: Request, res: Response) => {
+    res.sendFile(path + '/terms-of-service.html');
+});
+app.get('/about-us', (req: Request, res: Response) => {
+    res.sendFile(path + '/about-us.html');
+});
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(path + '/index.html');
+});
 app.listen(port);
